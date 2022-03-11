5135b5eaa841682a730a9bdbb5b5e633ace147f1
udaya276 <udaya276@gmail.com>
2022-03-11 11:44:48 +0530

    base model added

---
+++ src/stage_02_base_model_creation.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+import tensorflow as tf
+
+
+STAGE = "base model creation" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path):
+    ## read config files
+    config = read_yaml(config_path)
+
+    params = config["params"]
+
+    logging.info("layers defined")
+    LAYERS = [
+    tf.keras.layers.Input(shape=tuple(params["img_shape"])),
+    tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(8, activation="relu"),
+    tf.keras.layers.Dense(2, activation="softmax")
+]
+
+    classifier = tf.keras.Sequential(LAYERS)
+
+    logging.info(classifier.summary())
+
+    classifier.compile(
+    optimizer = tf.keras.optimizers.Adam(params["lr"]),
+    Loss = params["loss"],
+    metrics = params["metrics"]
+    )
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
